f8bb4a966fa2872a53a0337d2302cc03d08684ea
김도형 <angero@naver.com>
2020-04-07 19:38:09 +0900

    test commit

--- src/main.go
+++ src/main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type testStruct struct {
+	field1 string
+	field2 string
+}
+
+var globalNum int
+
+func test_Functoin() string {
+
+	return "test"
+}
+
+func testError() (string, error) {
+	return "test error", errors.New("test error")
+}
+
+func unused() {
+	println("unused")
+}
 
 func main(){
 
-	var testnum int
-	fmt.Sprintf("hello")
-	println("hello world")
+	//this is test commentary, langauge
+	fmt.Sprintf("hello langauge")
+
+	ts := &testStruct{
+		field1:"test",
+	}
+
+
+
+	testMsg, _ := testError()
+	println(testMsg)
+
+	println(test_Functoin())
+	println(test_Functoin())
+
+	println(ts.field1)
 
 }
